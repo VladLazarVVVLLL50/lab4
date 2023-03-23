e873970be7831310bf2aad9c6ba366d878ca9751
VladLazarVVVLLL50 <lazarv517@gmail.com>
2023-03-23 11:52:58 +0200

    last function

--- test.c
+++ test.c
@@ -2,16 +2,41 @@
 
 
 int main()
-{int *v=NULL;
- int n,i,x;
+{Node *v=NULL;
+ int n,i,x,j=0;
  scanf("%d",&n);
  for(i=0;i<n;i++)
  {scanf("%d",&x);
   addAtEnd(&v,x);
  }
+
+ Node *aux=v;
+
+ int k,value;
+
+ scanf("%d",&k);
+ for(i=0;i<k-1;i++)aux=aux->next;
+ Node *nou=(Node*)malloc(sizeof(Node));
+ scanf("%d",&value);
+ nou->val=value;
+ nou->next=aux->next;
+ aux->next=val;
+ n++;
+
+ for(i=0;i<n-1;i++)
+ {Node *aux1=aux->next;
+ int sum=0;
+  for(j=i+1;j<n;j++)
+  {
+   sum+=aux1->val;
+   aux1=aux1->next;
+  }
+   modify_value(&v,sum+aux->val,i);
+   aux=aux->next;
+ }
+
  display(v);
- modify_value(&v,12,3);
- display(v);
+
 
     return 0;
 }
